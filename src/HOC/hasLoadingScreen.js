a68e44371de3411e2a35a8b3e888aa54d6bdfe15
qcaodigital <qcao.digital@gmail.com>
2021-04-23 15:06:57 -0400

    change directory names

--- src/HOC/hasLoadingScreen.js
+++ src/HOC/hasLoadingScreen.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import LoadingScreen from '../Components/Common/LoadingScreen';
+import LoadingScreen from '../components/Common/LoadingScreen';
 
-function hasLoadingScreen(WrappedComponent){
-    function wrappedComponent(props){
-        let { componentMounted, loadingMessage, disableLoading } = props;
-        if(disableLoading || componentMounted){
-            return <WrappedComponent {...props} loaded={true} />
-        } else {
-            return <LoadingScreen loadingMessage={ loadingMessage } />
-        }
-    }
+function hasLoadingScreen(WrappedComponent) {
+	function wrappedComponent(props) {
+		let { componentMounted, loadingMessage, disableLoading } = props;
+		if (disableLoading || componentMounted) {
+			return <WrappedComponent {...props} loaded={true} />;
+		} else {
+			return <LoadingScreen loadingMessage={loadingMessage} />;
+		}
+	}
 
-    return wrappedComponent;
+	return wrappedComponent;
 }
 
 export default hasLoadingScreen;
